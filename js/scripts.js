8189ea0126b740e9c1d42863efc21252300ae957
Stephen weaver <104775681+stepweaver@users.noreply.github.com>
2022-07-14 14:33:13 -0400

    Update scripts.js
    added modal container and created elements

--- js/scripts.js
+++ js/scripts.js
@@ -64,9 +64,70 @@ let pokemonRepository = (function () {
 
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
-      console.log(pokemon);
+      showModal(pokemon);
     });
   }
+  // MODAL CONTAINER
+
+  let modalContainer = document.querySelector('#modal-container');
+  let modalClose = document.querySelector('#modal-close');
+  let modalTitle = document.querySelector('#modal-titel');
+  let modalContent = document.querySelector('#modal-content');
+
+  function showModal(pokemon) {
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let pokemonImg = document.createElement('img');
+    pokemonImg.classList.add('img-fluid');
+    pokemonImg.src = pokemon.imageUrl;
+
+    let titleElement = document.createElement('h2');
+    titleElement.classList.add('text-upper');
+    titleElement.innerText = pokemon.name;
+
+    let contentElement = document.createElement('p');
+    contentElement.classList.add('col');
+    contentElement.classList.add('text-center');
+    contentElement.innerText = 'Height: ' + pokemon.height;
+
+    let contentElement2 = document.createElement('p');
+    contentElement2.classList.add('col');
+    contentElement2.classList.add('text-center');
+    contentElement2.innerText = 'Weight: ' + pokemon.weight;
+
+    let contentElement3 = document.createElement('p');
+    contentElement3.classList.add('col');
+    contentElement3.classList.add('text-center');
+    contentElement3.innerText = 'Type: ' + pokemon.types;
+
+    let contentElement4 = document.createElement('p');
+    contentElement4.classList.add('col');
+    contentElement4.classList.add('text-center');
+    contentElement4.innerText = 'Abilitie(s) ' + pokemon.abilities;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
 
   return {
     add: add,
