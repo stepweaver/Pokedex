fcd9e64cb36fa674c7b1ed9e25fe5b360b1beb0e
Stephen weaver <104775681+stepweaver@users.noreply.github.com>
2022-07-27 13:33:17 -0400

    Update scripts.js
    backtracking

--- js/scripts.js
+++ js/scripts.js
@@ -76,7 +76,53 @@ let pokemonRepository = (function() {
     loadDetails(pokemon).then(function() {
       console.log(pokemon);
     });
-  }
+  } 
+
+  // MODAL
+  // function showModal(title, text) {
+  //   modalContainer.innerHTML = '';
+  //   let modal = document.createElement('div');
+  //   modal.classList.add('modal');
+
+  //   let closeButtonElement = document.createElement('button');
+  //   closeButtonElement.classList.add('modal-close');
+  //   closeButtonElement.innerText = 'Close';
+  //   closeButtonElement.addEventListener('click', hideModal);
+
+  //   let titleElement = document.createElement('h1');
+  //   titleElement.innerText = title;
+
+  //   let contentElement = document.createelement('p');
+  //   contentElement.innerText = text;
+
+  //   modal.appendChild(closeButtonElement);
+  //   modal.appendChild(titleElement);
+  //   modal.appendChild(contentElement);
+  //   modalContainer.appendChild(modal);
+
+  //   modalContainer.classList.add('is-visible');
+  // }
+
+  // function hideModal() {
+  //   modalContainer.classList.remove('is-visible');
+  // }
+
+  // window.addEventListener('keydown', (e) => {
+  //   if(e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+  //     hideModal();
+  //   }
+  // });
+
+  // modalContainer.addEventListener('click', (e) => {
+  //   let target = e.target;
+  //   if(target === modalContainer) {
+  //     hideModal();
+  //   }
+  // });
+
+  // document.querySelector('#show-modal').addEventListener('click', () => {
+  //   showModal(pokemon);
+  // });
 
   return {
     add: add,
