67401558de5b9821dfbadddfaeb4fa2c996c476d
Stephen weaver <104775681+stepweaver@users.noreply.github.com>
2022-07-31 11:23:00 -0400

    Update scripts.js
    added pokeCache so subsequent calls don't have to load from api

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,86 @@
+let pokedex = document.getElementById('pokedex');
+let pokeCache = {};
 
 // fetch to get data from pokeapi
-let fetchPokemon = () => {
-  let pokemonList = [];
-  for( let i = 1; i <= 150; i++) { // iterate pokemon 1 through 150
-    let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    pokemonList.push(fetch(url).then( res => res.json()));
-  }
-  
-  Promise.all(pokemonList).then( results => {
-    let pokemon = results.map( data => ({ // iterate through each result and returns new array of objects
-      name: data.name,
-      id: data.id,
-      image: data.sprites['front_default'],
-      height: data.height + ' dm',
-      weight: data.weight + ' hg',
-      type: data.types.map( type => type.type.name).join(', ')
+let fetchPokemon = async () => {
+  let url = `https://pokeapi.co/api/v2/pokemon/?limit=150`;
+  let res = await fetch(url);
+  let data = await res.json();
+    let pokemon = data.results.map ( (result, index) =>
+    ({
+      name: result.name,
+      apiUrl: result.url,
+      id: index +1,
+      image: `https://raw.githubusercontent.com/pokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
     }));
     displayPokemon(pokemon);
-  });
 };
 
 let displayPokemon = (pokemon) => {
-  console.log(pokemon);
-}
+  // create list item for each pokemon
+  let pokemonHTMLString = pokemon.map ( pokemonList => `
+  <li class="card" onclick="selectPokemon(${pokemonList.id})">
+    <img class="card-image" src='${pokemonList.image}' />
+    <h2 class="card-title">${pokemonList.name}</h2>
+  </li>
+  `)
+  .join('');
+  pokedex.innerHTML = pokemonHTMLString;
+};
+
+let selectPokemon = async (id) => {
+  if(!pokeCache[id]){
+  let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  let res = await fetch(url);
+  let pokemonList = await res.json();
+  pokeCache[id] = pokemonList;
+  showModal(pokemonList);
+  }
+  showModal(pokeCache[id]);
+};
+
+let showModal = (pokemonList) => {
+  let type = pokemonList.types.map( type => type.type.name).join(', ');
+  let image = pokemonList.sprites['front_default'];
+  let htmlString = `
+    <div class="modal">
+      <button id="modal-close" onclick="closeModal()">Close</button>
+      <div class="card">
+        <img class="card-image" src='${image}' />
+        <h2 class="card-title">${pokemonList.name}</h2>
+        <p>
+          <small>Height: </small>${pokemonList.height} dm
+          | <small>Weight: </small>${pokemonList.weight} hg
+          | <small>Type: </small>${type}
+        </p>
+      </div>
+    </div>
+   `;
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      closeModal();
+    }
+  });
+  
+  // // let  closeButtonElement = document.createElement('button');
+  // // closeButtonElement.classList.add('modal-close');
+  // // closeButtonElement.innerText = 'Close';
+  // // closeButtonElement.addEventListener('click', closeModal);
+
+  // // window.addEventListener('keydown', (e) => {
+  // //   if (e.key === 'Escape') {
+  // //     closeModal();
+  // //   }
+  // };
+
+  pokedex.innerHTML = htmlString +  pokedex.innerHTML;
+  console.log(htmlString);
+};
+
+let closeModal = () => {
+  let modal = document.querySelector('.modal');
+  // modal.parentElement.removeChild(modal); FIX THIS!!!!!
+};
 
 fetchPokemon();
